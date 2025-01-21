9681195f0afd6450e119d365be8e24dc6b582454
01-13-99 <sofiaeffio@gmail.com>
2025-01-21 08:25:11 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+let inputEl = document.getElementById("input-el")
+let btnEl = document.getElementById("btn-el")
+
+btnEl.addEventListener("click", function(){
+  console.log("click")
+})
